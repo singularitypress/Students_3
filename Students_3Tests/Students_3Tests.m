b51d5beead25c916ca33752da2c7b749228e5d7f
singularitypress <jaypandya2k5@gmail.com>
2016-02-01 18:29:36 -0500

    Initial Commit

---
+++ Students_3Tests/Students_3Tests.m
@@ -0,0 +1,39 @@
+//
+//  Students_3Tests.m
+//  Students_3Tests
+//
+//  Created by Jay on 2016-02-01.
+//  Copyright © 2016 Jay. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Students_3Tests : XCTestCase
+
+@end
+
+@implementation Students_3Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
